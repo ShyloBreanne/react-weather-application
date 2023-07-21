62862061b4fd598045b55ba836f39f28269db593
ShyloBreanne <sblyall@gmail.com>
2023-07-21 11:07:20 -0700

    Added Netlify URL link

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ export default function App() {
       <Weather defaultCity="Portland"/>
       <footer>
         This project was coded by <a href="https://www.linkedin.com/in/breannelyall/">Breanne Lyall</a> and is {""}
-        <a href="https://github.com/ShyloBreanne/react-weather-application" target="_blank">open-sourced</a> on GitHub
+        <a href="https://github.com/ShyloBreanne/react-weather-application" target="_blank">open-sourced</a> on GitHub and <a href="https://poetic-sunshine-74c8fb.netlify.app/" target="_blank">hosted on Netlify</a>
         </footer>
       </div>
     </div>
