6d00301d63b0361232bc0d5b85383de92ff2cb46
ShyloBreanne <sblyall@gmail.com>
2023-07-22 13:06:53 -0700

    Set up the weather forecast

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -6,23 +6,25 @@ import WeatherTemperature from "./WeatherTemperature";
 export default function WeatherInfo(props) {
     return (
         <div className="WeatherInfo">
-        <h1>{props.data.city}</h1>
-            <ul>
-                <li><FormattedDate date={props.data.date} /></li>
-                <li className="text-capitalize">{props.data.description}</li>
-            </ul>
+            <h1>{props.data.city}</h1>
+                <ul>
+                    <li><FormattedDate date={props.data.date} /></li>
+                    <li className="text-capitalize">{props.data.description}</li>
+                </ul>
+
             <div className="row mt-3">
-                <div className="col-6">
-                    <WeatherIcon code={props.data.icon}/>
+               <div className="col-6">
+                    <WeatherIcon code={props.data.icon} size={60} />
                     <WeatherTemperature fahrenheit={props.data.temperature} />
-                    </div>
-                <div className="col-6">
-                    <ul>
-                        <li>Humidity: {props.data.humidity}%</li>
-                        <li>Wind: {Math.round(props.data.wind)} MpH</li>
-                    </ul>
                 </div>
+                <div className="col-6">
+                        <ul>
+                            <li>Humidity: {props.data.humidity}%</li>
+                            <li>Wind: {Math.round(props.data.wind)} MpH</li>
+                        </ul>
+                    </div>
+                </div>
+                
             </div>
-        </div>
     );
 }
