00073fad262e8a449b7be102b680a8150977bda8
ShyloBreanne <sblyall@gmail.com>
2023-07-25 12:10:58 -0700

    Added noreferrer tag to links

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ export default function App() {
       <div className="container">
       <Weather defaultCity="Portland"/>
       <footer>
-        This project was coded by <a href="https://www.linkedin.com/in/breannelyall/">Breanne Lyall</a> and is {""}
-        <a href="https://github.com/ShyloBreanne/react-weather-application" target="_blank">open-sourced</a> on GitHub and <a href="https://poetic-sunshine-74c8fb.netlify.app/" target="_blank">hosted on Netlify</a>
+        This project was coded by <a href="https://www.linkedin.com/in/breannelyall/" target="_blank" rel="noopener noreferrer">Breanne Lyall</a> and is {""}
+        <a href="https://github.com/ShyloBreanne/react-weather-application" target="_blank" rel="noopener noreferrer">open-sourced</a> on GitHub and <a href="https://poetic-sunshine-74c8fb.netlify.app/" target="_blank" rel="noopener noreferrer">hosted on Netlify</a>
         </footer>
       </div>
     </div>
