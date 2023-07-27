c1d7d8b08a10793390f4cc908acf837f90434323
ShyloBreanne <sblyall@gmail.com>
2023-07-27 08:57:09 -0700

    Updated broken Netlify link

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ export default function App() {
       <footer>
           This project was coded by <a href="https://www.linkedin.com/in/breannelyall/" target="_blank" rel="noopener noreferrer" title="Linkedin">Breanne Lyall.</a>
           <br />
-          It is open-sourced on{" "}<a href="https://github.com/ShyloBreanne/react-weather-application" target="_blank" rel="noopener noreferrer" title="GitHub">GitHub</a> and hosted on{" "}<a href="https://poetic-sunshine-74c8fb.netlify.app/" target="_blank" rel="noopener noreferrer" title="Netlify">Netlify</a>{" "}💜
+          It is open-sourced on{" "}<a href="https://github.com/ShyloBreanne/react-weather-application" target="_blank" rel="noopener noreferrer" title="GitHub">GitHub</a> and hosted on{" "}<a href="https://react-weather-application-breanne.netlify.app/" target="_blank" rel="noopener noreferrer" title="Netlify">Netlify</a>{" "}💜
         </footer>
       </div>
     </div>
