9df26b545f7820cfe840cd069998cccfa505fe3d
ShyloBreanne <sblyall@gmail.com>
2023-07-27 08:43:01 -0700

    Updated meta description, added titles to links

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ export default function App() {
       <div className="container">
       <Weather defaultCity="Portland"/>
       <footer>
-          This project was coded by <a href="https://www.linkedin.com/in/breannelyall/" target="_blank" rel="noopener noreferrer">Breanne Lyall.</a>
+          This project was coded by <a href="https://www.linkedin.com/in/breannelyall/" target="_blank" rel="noopener noreferrer" title="Linkedin">Breanne Lyall.</a>
           <br />
-          It is open-sourced on{" "}<a href="https://github.com/ShyloBreanne/react-weather-application" target="_blank" rel="noopener noreferrer">GitHub</a> and hosted on{" "}<a href="https://poetic-sunshine-74c8fb.netlify.app/" target="_blank" rel="noopener noreferrer">Netlify</a>{" "}💜
+          It is open-sourced on{" "}<a href="https://github.com/ShyloBreanne/react-weather-application" target="_blank" rel="noopener noreferrer" title="GitHub">GitHub</a> and hosted on{" "}<a href="https://poetic-sunshine-74c8fb.netlify.app/" target="_blank" rel="noopener noreferrer" title="Netlify">Netlify</a>{" "}💜
         </footer>
       </div>
     </div>
